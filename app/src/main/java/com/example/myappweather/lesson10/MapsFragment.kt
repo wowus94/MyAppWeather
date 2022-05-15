855275e71e6e772bf3bd17276fef9d09cea0d472
wowus94 <sesila94@mail.ru>
2022-05-15 22:47:20 +0300

    ДЗ 10: настроил интерфейс карт

--- app/src/main/java/com/example/myappweather/lesson10/MapsFragment.kt
+++ app/src/main/java/com/example/myappweather/lesson10/MapsFragment.kt
@@ -1,31 +1,76 @@
 package com.example.myappweather.lesson10
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.inflate
 import android.view.ViewGroup
-import androidx.appcompat.resources.Compatibility.Api21Impl.inflate
-import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.myappweather.R
-import com.example.myappweather.databinding.ActivityMainBinding.inflate
-import com.example.myappweather.databinding.FragmentDetailsBinding.inflate
 import com.example.myappweather.databinding.FragmentMapsMainBinding
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
 class MapsFragment : Fragment() {
-
-
     private val callback = OnMapReadyCallback { googleMap ->
-
+        map = googleMap
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map.setOnMapLongClickListener {
+            addMarkerToArray(it)
+            drawLine()
+        }
+        map.uiSettings.isZoomControlsEnabled = true
+        map.uiSettings.isMyLocationButtonEnabled = true
+
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+            map.isMyLocationEnabled = true
+    }
+
+    private lateinit var map: GoogleMap
+    private val markers: ArrayList<Marker> = arrayListOf()
+
+    private fun addMarkerToArray(location: LatLng) {
+        val marker = setMarker(location, markers.size.toString(), R.drawable.ic_map_pin)
+        markers.add(marker)
+    }
+
+    fun drawLine() {
+        var previousBefore: Marker? = null
+        markers.forEach { current ->
+            previousBefore?.let { previous ->
+                map.addPolyline(
+                    PolylineOptions().add(previous.position, current.position)
+                        .color(Color.RED)
+                        .width(5f)
+                )
+            }
+            previousBefore = current
+        }
+    }
+
+    private fun setMarker(
+        location: LatLng,
+        searchText: String,
+        resourceId: Int
+    ): Marker {
+        return map.addMarker(
+            MarkerOptions()
+                .position(location)
+                .title(searchText)
+                .icon(BitmapDescriptorFactory.fromResource(resourceId))
+        )!!
     }
 
     private var _binding: FragmentMapsMainBinding? = null
